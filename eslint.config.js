ecdf6836441f88b237330cb7ac9a9ebd595ff369
Nándor Dudás <nandor.dudas@gmail.com>
2023-11-05 13:06:49 +0000

    chore: update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,20 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['devcontainer.json'],
+  ignores: ['**/devcontainer.json'],
+  rules: {
+    'complexity': ['error', 5],
+    'import/no-anonymous-default-export': 'warn',
+    'import/order': ['error', {
+      'alphabetize': { caseInsensitive: true, order: 'asc' },
+      'groups': ['external', 'builtin', ['sibling', 'parent'], 'index', 'object'],
+      'newlines-between': 'always',
+      'pathGroups': [
+        // You need to start your aliases with "~/".
+        { group: 'external', pattern: '~/**', position: 'after' },
+      ],
+    }],
+    'max-len': ['error', { code: 120, comments: 80, ignoreStrings: true, ignoreUrls: true }],
+    'perfectionist/sort-objects': 'error',
+  },
 })
